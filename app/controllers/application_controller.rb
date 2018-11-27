b4175992e3cb1cc1a421986321bdb03fddcb64d7
Harley Rayner <harleymteden@gmail.com>
2018-11-27 19:43:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,21 +13,19 @@ class App < Sinatra::Base
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       binding.pry
 
-      #iterates over the heroes hash
+      #iterates over the params hash (member1, member2, member3)
       params[:team][:heroes][0].each_with_index do |hero_data, index|
         hero = Hero.new
         index += 1
-        # binding.pry
+
         hero.name = hero_data["member#{index}".to_sym]["member#{index}_name".to_sym]
         hero.power = hero_data["member#{index}".to_sym]["member#{index}_power".to_sym]
         hero.bio = hero_data["member#{index}".to_sym]["member#{index}_bio".to_sym]
         @team.heroes << hero
-        # binding.pry
-        # binding.pry
-        # binding.pry
+
       end
       @team
-        # binding.pry
+
       erb :show
     end
 
