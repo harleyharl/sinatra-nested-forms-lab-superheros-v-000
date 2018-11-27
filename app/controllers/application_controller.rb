c68323d5b02969be99befc8a69008d0abdd46dae
Harley Rayner <harleymteden@gmail.com>
2018-11-27 19:49:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
       #iterates over the params hash (member1, member2, member3)
       params[:team][:heroes].each_with_index do |member, index|
         hero = Hero.new
-        index += 1
+        # index += 1
         hero.name = member["member#{index}"]["member#{index}_name"]
         hero.power = member["member#{index}"]["member#{index}_power"]
         hero.bio = member["member#{index}"]["member#{index}_bio"]
